ebbd83111e2800ea6d44dee1ae35d9967923020a
KARTHIKDEGALA18 <degalakarthik14@gmail.com>
2024-11-15 14:43:12 +0530

    Uploading code: Sum it.c

---
+++ Sum it.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+    int A,B,C;
+    scanf("%d%d%d", &A,&B,&C);
+    if(A+B==C)
+    {
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+}
